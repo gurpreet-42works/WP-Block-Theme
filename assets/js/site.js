d156e2ef6c238b85a9241d79e7dea99350e7fedc
gurpreet-42works <gurpreet_kaler@42works.net>
2025-08-11 13:40:14 +0530

    Added dynamic social icons

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,13 @@
             var bsTarget = $(this).data('bs-target');
             $(bsTarget).toggleClass('active');
         });
+
+
+        //Add a class if there are no social icons in bottom footer
+        if( $("#footer-row-bottom").find(".footer-social-nav").length == 0 ){
+            $("#footer-row-bottom").addClass('no-social');
+        }
+        
    });
 
 
--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -51,9 +51,69 @@ function bloxby_contact_form_shortcode()
     ob_start();
 ?>
     <div class="bloxby-contact-form py-4">
-        <?php echo do_shortcode( '[contact-form-7 title="Bloxby Contact Form"]' ); ?>
+        <?php echo do_shortcode('[contact-form-7 title="Bloxby Contact Form"]'); ?>
     </div>
 <?php
     return ob_get_clean();
 }
 add_shortcode('bloxby_contact_form', 'bloxby_contact_form_shortcode');
+
+
+function bloxby_social_icons_shortcode()
+{
+    ob_start();
+
+    $site_data = get_option('sitedata', '');
+    if (!empty($site_data)) {
+        $data_array = unserialize(base64_decode($site_data));
+
+        $show_icons = false;
+        $icons_html = '';
+        foreach ($data_array['social_urls'] as $key => $value) {
+            if ( !empty($value) ) {
+                $show_icons = true;
+                switch ($key) {
+                    case 'facebook':
+                        $icons_html .= '<li class="ms-3">
+                            <a class="text-body-secondary" href="' . $value . '" aria-label="Facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                        </li>';
+                        break;
+                    case 'instagram':
+                        $icons_html .= '<li class="ms-3">
+                            <a class="text-body-secondary" href="' . $value . '" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a>
+                        </li>';
+                        break;
+                    case 'twitter':
+                        $icons_html .= '<li class="ms-3">
+                            <a class="text-body-secondary" href="' . $value . '" aria-label="X"><i class="fa-brands fa-x-twitter"></i></a>
+                        </li>';
+                        break;
+                    case 'linkedin':
+                        $icons_html .= '<li class="ms-3">
+                            <a class="text-body-secondary" href="' . $value . '" aria-label="linkedin"><i class="fa-brands fa-linkedin"></i></a>
+                        </li>';
+                        break;
+                    case 'pinterest':
+                        $icons_html .= '<li class="ms-3">
+                            <a class="text-body-secondary" href="' . $value . '" aria-label="Pinterest"><i class="fa-brands fa-pinterest"></i></a>
+                        </li>';
+                        break;
+                    default:
+                        $icons_html .= '';
+                        break;
+                }
+            } else {
+                $show_icons = false;
+            }
+        }
+
+        if ($show_icons) {
+            echo '<ul class="footer-social-nav nav col-md-5 col-12 justify-content-md-end justify-content-center list-unstyled d-flex m-0">';
+            echo $icons_html;
+            echo '</ul>';
+        }
+    }
+
+    return ob_get_clean();
+}
+add_shortcode('bloxby_social_icons', 'bloxby_social_icons_shortcode');
