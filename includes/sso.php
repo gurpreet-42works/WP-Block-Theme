282fbe5cde0e9910e772da15a0c8776bba03845d
gurpreet-42works <gurpreet_kaler@42works.net>
2025-09-25 16:50:37 +0530

    Fix: SSO changes

--- includes/sso.php
+++ includes/sso.php
@@ -12,22 +12,25 @@ function handle_auto_login(WP_REST_Request $request)
 {
 
     $payload = $request->get_param('payload');
+    $email = $request->get_param('email');
     $site_id = get_option( 'boxby_siteid', false );
 
+
+    
     if (!$payload || !$site_id) {
         return new WP_REST_Response(['error' => 'Missing parameters'], 400);
     }
 
     $secret_key = 'Bloxby_website_#000009';
-    $site_id = 'site_id_' . $site_id;
+    $site_key = 'site_id_' . $site_id . '|email_id_' . $email;
 
-    echo $calculated_signature = hash_hmac('sha256', $site_id, $secret_key);
+    $calculated_signature = hash_hmac('sha256', $site_key, $secret_key);
    
     if ( !hash_equals($calculated_signature, $payload) ) {
         return new WP_REST_Response(['error' => 'Invalid signature'], 403);
     }
 
-    $user = get_user_by('ID', 1);
+    $user = get_user_by('email', $email);
     if (!$user) {
         return new WP_REST_Response(['error' => 'User not found'], 404);
     }
